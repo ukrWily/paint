16eea94e8d5290892ed369bebbfb5c743e3814c3
ukrWily <ukrwily@gmail.com>
2024-06-14 20:27:25 +0300

    L range

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-document.querySelectorAll(".color").forEach((item) => {
-  item.style.backgroundColor = item.getAttribute("data-color");
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".color").forEach((item) => {
+    item.style.backgroundColor = item.getAttribute("data-color");
+  });
+
+  const thin = document.querySelector(".thin-line > input");
+  thin.addEventListener("change", () => {
+    const line = thin.value;
+    document.querySelector(
+      ".thin-line > label"
+    ).textContent = `Thin of line - ${line}px`;
+  });
+
+  const canvas = document.querySelector("#canvas");
+  let painting = false;
+
+  function stopPainting() {
+    painting = false;
+  }
+
+  function onMouseMove(event) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+  }
+
+  function onMouseDown(event) {
+    painting = true;
+  }
+
+  function onMouseUp(event) {
+    stopPainting();
+  }
+
+  if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseUp", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+  }
 });
-
-const canvas = document.querySelector("#canvas");
-let painting = false;
-
-function stopPainting () {
-  painting = false;
-};
-
-function onMouseMove(event) {
-  let x = event.offsetX;
-  let y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
-}
-
-
-if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseUp", onMouseUp);
-  canvas.addEventListener("mouseleave", stopPainting;
-}
