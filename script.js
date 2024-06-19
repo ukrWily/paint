6a7b8fd53bd9df52d114350767e77a7bf3f3c6d9
ukrWily <ukrwily@gmail.com>
2024-06-19 13:49:43 +0300

    L filling

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.querySelector("#canvas");
-  /**
-   * get canvas context
-   */
-  const ctx = canvas.getContext("2d");
-
-  canvas.width = 700;
-  canvas.height = 700;
-  /**
-   * set thin of line
-   * */
-  ctx.lineWidth = 2.5;
-  /**
-   * set color of line
-   */
-  ctx.strokeStyle = "#2c2c2c";
-
-  let painting = false;
-
-  const colors = document.querySelectorAll(".color");
-  colors.forEach((color) => {
-    color.style.backgroundColor = color.getAttribute("data-color");
-    color.addEventListener("click", changeColor);
-  });
-
-  const thin = document.querySelector(".thin-line > input");
-
-  thin.addEventListener("change", () => {
-    const line = thin.value;
-    document.querySelector(".range-line").style.height = `${line}px`;
-    ctx.lineWidth = line;
-    document.querySelector(
-      ".thin-line > label"
-    ).textContent = `Thin of line - ${line}px`;
-  });
-
-  document.querySelector(".btn-clear").addEventListener("click", clearCanvas);
-  //<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>
-
   function stopPainting() {
     painting = false;
   }
@@ -76,16 +37,86 @@ window.addEventListener("DOMContentLoaded", () => {
   function changeColor(event) {
     const color = event.target.getAttribute("data-color");
     ctx.strokeStyle = color;
+    rangeLine.style.backgroundColor = color;
+    mode.style.backgroundColor = color;
+    ctx.fillStyle = color;
   }
 
   function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
+  function handleMode() {
+    if (filling) {
+      filling = false;
+      mode.innerText = "Drawing";
+      mode.classList.remove("oval");
+      mode.classList.add("rectangle");
+    } else {
+      filling = true;
+      mode.innerText = "Filling";
+      mode.classList.add("oval");
+      mode.classList.remove("rectangle");
+    }
+  }
+
+  function fillingCanvas() {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+
+  //<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>
+  const canvas = document.querySelector("#canvas");
+  const rangeLine = document.querySelector(".range-line");
+  const mode = document.querySelector(".btn-mode");
+  const INITIAL_COLOR = "#2c2c2c";
+  const CANVAS_SIZE = 700;
+  /**
+   * get canvas context
+   */
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = CANVAS_SIZE;
+  canvas.height = CANVAS_SIZE;
+  /**
+   * set thin of line
+   * */
+  ctx.lineWidth = 2.5;
+  /**
+   * set color of line
+   */
+  ctx.strokeStyle = INITIAL_COLOR;
+  ctx.fillStyle = INITIAL_COLOR;
+
+  let painting = false;
+  let filling = false;
+
+  const colors = document.querySelectorAll(".color");
+  colors.forEach((color) => {
+    color.style.backgroundColor = color.getAttribute("data-color");
+    color.addEventListener("click", changeColor);
+  });
+
+  const thin = document.querySelector(".thin-line > input");
+
+  thin.addEventListener("change", () => {
+    const line = thin.value;
+    rangeLine.style.height = `${line}px`;
+    ctx.lineWidth = line;
+    document.querySelector(
+      ".thin-line > label"
+    ).textContent = `Thin of line - ${line}px`;
+  });
+
+  document.querySelector(".btn-clear").addEventListener("click", clearCanvas);
+  //<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<{<>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>}>
+
   if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", fillingCanvas);
   }
+
+  mode.addEventListener("click", handleMode);
 });
